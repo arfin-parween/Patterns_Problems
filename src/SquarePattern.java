d5d1e53f2abc0676ffa4c28fc8a669011221db0d
Arfin Parween <wbstartpracticing@gmail.com>
2024-01-07 11:39:19 +0530

    SquarePattern

--- src/SquarePattern.java
+++ src/SquarePattern.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
-
+/*
+Print Square pattern like this
+ * * * * * *
+ * * * * * *
+ * * * * * *
+ * * * * * *
+ */
 public class SquarePattern {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
